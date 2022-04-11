4faf4aa2f31281b044a2ae52cacc6eea0d6ffd0a
Animesh Okhade <96253880+animeshokhade@users.noreply.github.com>
2022-04-11 23:28:34 +0530

    Add files via upload

---
+++ leetcode/#1920.py
@@ -0,0 +1,14 @@
+# question --> https://leetcode.com/problems/build-array-from-permutation/
+
+class Solution:
+    def buildArray(self, nums: List[int]) -> List[int]:
+        iteration = len(nums)
+        ans = list()
+        for i in range(iteration):
+            ans.append(nums[nums[i]])
+        return ans
+        
+        # TC: O(N); SC: O(N) 
+
+# end
+        
