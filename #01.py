239bd85cb1ad401b406723d68201214c15215664
Animesh Okhade <96253880+animeshokhade@users.noreply.github.com>
2022-04-13 09:19:15 +0530

    Delete #01.py

--- #01.py
+++
@@ -1,8 +0,0 @@
-# question --> https://practice.geeksforgeeks.org/problems/print-alternate-elements-of-an-array/1
-
-def printAl(arr,n):
-    # your code here
-    for i in range(0, len(arr), 2):
-        print(arr[i], end = ' ')
-        
-    # TC: O(N); SC: O(1)
