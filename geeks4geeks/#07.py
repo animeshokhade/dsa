719d45d7cb38e36d1f39d4d6e6a92c7d4f294e14
Animesh Okhade <96253880+animeshokhade@users.noreply.github.com>
2022-04-20 13:40:15 +0530

    Add files via upload

---
+++ geeks4geeks/#07.py
@@ -0,0 +1,8 @@
+# question --> https://practice.geeksforgeeks.org/problems/largest-element-in-array4009/1/?page=1&difficulty[]=-1&category[]=Arrays&sortBy=submissions
+
+def largest( arr, n):
+    return max(arr)
+    
+    # TC: O(N); SC: O(1) 
+    
+# end
