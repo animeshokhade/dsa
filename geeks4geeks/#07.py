de3b09f685c160ef9007c871a9a31b1a44a89d18
Animesh Okhade <96253880+animeshokhade@users.noreply.github.com>
2022-04-20 13:40:45 +0530

    Update #07.py

--- geeks4geeks/#07.py
+++ geeks4geeks/#07.py
@@ -1,6 +1,6 @@
 # question --> https://practice.geeksforgeeks.org/problems/largest-element-in-array4009/1/?page=1&difficulty[]=-1&category[]=Arrays&sortBy=submissions
 
-def largest( arr, n):
+def largest(arr, n):
     return max(arr)
     
     # TC: O(N); SC: O(1) 
