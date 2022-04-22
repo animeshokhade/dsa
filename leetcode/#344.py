8ca9022a7c6595475d0775eca2fb77b01df38c88
Animesh Okhade <96253880+animeshokhade@users.noreply.github.com>
2022-04-22 21:58:12 +0530

    Add files via upload

---
+++ leetcode/#344.py
@@ -0,0 +1,17 @@
+# question --> https://leetcode.com/problems/reverse-string/
+
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        start, end = 0, len(s) - 1
+        while start <= end: 
+            s[start], s[end] = s[end], s[start]
+            start += 1
+            end -= 1
+        return s
+        
+        # TC: O(S); SC: O(1)
+        
+# end 
