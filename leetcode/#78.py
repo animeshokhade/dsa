63e18e2750d27924c71b4e6b7239c1d01ff5a64a
Animesh Okhade <96253880+animeshokhade@users.noreply.github.com>
2022-04-26 20:13:20 +0530

    Add files via upload

---
+++ leetcode/#78.py
@@ -0,0 +1,66 @@
+# question --> https://leetcode.com/problems/subsets/
+
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        n = len(nums)
+        bitMapCount = 1 << n
+        
+        ans = [[]]
+        
+        for bitRep in range(1, bitMapCount):
+            subSet = []
+            bitTracker = 0
+            while bitRep: 
+                if bitRep & 1: 
+                    subSet.append(nums[bitTracker])
+                bitRep >>= 1
+                bitTracker += 1
+            ans.append(sorted(subSet))
+        
+        return sorted(ans)
+        
+        # TC: O(2^N); SC: O(N)
+        
+# Alternative Approach
+
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        ans = [[]]
+        
+        for num in nums: 
+            ans += [x + [num] for x in ans]
+            
+        return sorted(ans)
+        
+        # TC: O(2^N); SC: O(N) 
+        
+# Recursion Approach
+
+ans=[]
+class Solution:
+   
+    def helper(self, nums, temp, index):
+        
+        if index == len(nums):
+            ans.append(temp.copy())
+            return
+        self.helper(nums, temp, index + 1)
+        temp.append(nums[index])
+        self.helper(nums, temp, index + 1)
+        temp.pop()
+        
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+       
+        l = []
+        ans.clear()
+        nums.sort()
+        self.helper(nums, l, 0)
+        ans.sort()
+        return ans
+        
+        # TC: O(2^N); SC: O(N)
+        
+# end 
+
+
