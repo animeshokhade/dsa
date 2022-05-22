e2164583bd9242d7d30816dc787577b512031b54
Animesh Okhade <96253880+animeshokhade@users.noreply.github.com>
2022-05-22 17:19:32 +0530

    Add files via upload

---
+++ geeks4geeks/#08.py
@@ -0,0 +1,7 @@
+# question --> https://practice.geeksforgeeks.org/problems/find-minimum-and-maximum-element-in-an-array4428/1/?page=1&curated[]=8&sortBy=submissions
+
+def getMinMax(a, n):
+	return min(a), max(a)
+
+# TC: O(N); SC: O(1)
+# end
