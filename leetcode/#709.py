0d372436d62f4bc5a7ae8e426f654a075dba8539
Animesh Okhade <96253880+animeshokhade@users.noreply.github.com>
2022-05-25 00:50:56 +0530

    Add files via upload

---
+++ leetcode/#709.py
@@ -0,0 +1,8 @@
+# question --> https://leetcode.com/problems/to-lower-case/
+
+class Solution:
+    def toLowerCase(self, s: str) -> str:
+        return s.lower()        
+
+# TC: O(N); SC: O(1)
+# end 
