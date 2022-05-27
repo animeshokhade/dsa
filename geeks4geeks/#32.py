e6a4333377e156c549243823c5cbc451a7602c03
Animesh Okhade <96253880+animeshokhade@users.noreply.github.com>
2022-05-27 09:18:59 +0530

    Add files via upload

---
+++ geeks4geeks/#32.py
@@ -0,0 +1,9 @@
+# question --> https://practice.geeksforgeeks.org/problems/addition-of-two-numbers0812/1/?page=1&difficulty[]=-2&status[]=unsolved&sortBy=submissions#
+
+class Solution:
+    def addition(ob, A, B):
+        # code here
+        return A + B
+
+    # TC: O(1); SC: O(1)
+# end
