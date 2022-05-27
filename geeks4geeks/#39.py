d98f8c21b9235b61ca5b9e94a1010c42743e0090
Animesh Okhade <96253880+animeshokhade@users.noreply.github.com>
2022-05-27 12:23:49 +0530

    Add files via upload

---
+++ geeks4geeks/#39.py
@@ -0,0 +1,9 @@
+# question --> https://practice.geeksforgeeks.org/problems/remainder-evaluation3755/1/?page=1&difficulty[]=-2&status[]=unsolved&sortBy=submissions#
+
+class Solution:
+    def findRemainder(self, num1, num2):
+        # code here
+        return num1 % num2
+
+    # TC: O(1); SC: O(1)
+# end
