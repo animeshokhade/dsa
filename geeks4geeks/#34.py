a6ac9c5391c990fb4b060e71edec4187cd878770
Animesh Okhade <96253880+animeshokhade@users.noreply.github.com>
2022-05-27 09:27:21 +0530

    Add files via upload

---
+++ geeks4geeks/#34.py
@@ -0,0 +1,11 @@
+# question --> https://practice.geeksforgeeks.org/problems/print-elements-of-array4910/1/?page=1&difficulty[]=-2&status[]=unsolved&sortBy=submissions#
+
+class Solution:
+    # Just print the space seperated array elements
+    def printArray(self, arr, n):
+        # code here
+        for a in arr:
+            print(a, end=' ')
+
+# TC: O(N); SC: O(1)
+# end
