4758b72fb529dfaa22ba663747efff01b221378a
Animesh Okhade <96253880+animeshokhade@users.noreply.github.com>
2022-05-27 15:17:11 +0530

    Add files via upload

---
+++ geeks4geeks/#47.py
@@ -0,0 +1,9 @@
+# question --> https://practice.geeksforgeeks.org/problems/convert-a-list-of-characters-into-a-string5142/1/?page=2&difficulty[]=-2&status[]=unsolved&sortBy=submissions#
+
+class Solution:
+    def chartostr(self, arr, N):
+        # code here
+        return ''.join(arr)
+
+    # TC: O(N); SC: O(N)
+# end
