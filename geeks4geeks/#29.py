cf605bd69f279d4e3dee7188eb0ef1878e79685a
Animesh Okhade <96253880+animeshokhade@users.noreply.github.com>
2022-05-27 08:59:05 +0530

    Add files via upload

---
+++ geeks4geeks/#29.py
@@ -0,0 +1,10 @@
+# question --> https://practice.geeksforgeeks.org/problems/sum-of-series2811/1/?page=1&difficulty[]=-2&status[]=unsolved&sortBy=submissions#
+
+class Solution:
+
+    def seriesSum(self, n):
+        # code here
+        return ((n) * (n + 1)) // 2
+
+    # TC: O(1); SC: O(1)
+# end
