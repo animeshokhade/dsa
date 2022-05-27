57926a9f07a9f4d735c755afc67fa60594b8509a
Animesh Okhade <96253880+animeshokhade@users.noreply.github.com>
2022-05-27 13:45:11 +0530

    Add files via upload

---
+++ geeks4geeks/#46.py
@@ -0,0 +1,10 @@
+# question --> https://practice.geeksforgeeks.org/problems/swap-two-numbers3844/1/?page=1&difficulty[]=-2&status[]=unsolved&sortBy=submissions#
+
+class Solution:
+    def get(self, a, b):
+        # code here
+        a, b = b, a
+        return a, b
+
+    # TC: O(1); SC:O(1)
+# end
