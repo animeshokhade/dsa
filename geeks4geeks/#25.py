14c9fe6e2db69e18a61892e51312db65e80363bb
Animesh Okhade <96253880+animeshokhade@users.noreply.github.com>
2022-05-27 07:33:03 +0530

    Add files via upload

---
+++ geeks4geeks/#25.py
@@ -0,0 +1,9 @@
+# question --> https://practice.geeksforgeeks.org/problems/c-arrays-sum-of-array-set-14805/1/?page=1&difficulty[]=-2&status[]=unsolved&sortBy=submissions#
+
+class Solution:
+    def getSum(self, arr, n):
+        # Your code goes here
+        return sum(arr)
+
+    # TC: O(N); SC: O(1)
+# end
