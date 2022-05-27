6dc17669ce030b601c208b7e7241bbb501566a14
Animesh Okhade <96253880+animeshokhade@users.noreply.github.com>
2022-05-27 10:03:53 +0530

    Add files via upload

---
+++ geeks4geeks/#35.py
@@ -0,0 +1,9 @@
+# question --> https://practice.geeksforgeeks.org/problems/java-convert-string-to-lowercase2313/1/?page=1&difficulty[]=-2&status[]=unsolved&sortBy=submissions#
+
+class Solution:
+    def toLower(ob, S):
+        # code here
+        return S.lower()
+
+    # TC: O(N); SC: O(1)
+# end
