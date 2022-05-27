5090409554b53fe37a2b822dadddbd3ad7efb9a3
Animesh Okhade <96253880+animeshokhade@users.noreply.github.com>
2022-05-27 09:11:55 +0530

    Add files via upload

---
+++ geeks4geeks/#30.py
@@ -0,0 +1,10 @@
+# question --> https://practice.geeksforgeeks.org/problems/sum-of-array2326/1/?page=1&difficulty[]=-2&status[]=unsolved&sortBy=submissions#
+
+class Solution:
+
+    def _sum(self, arr, n):
+        # code here
+        return sum(arr)
+
+# TC: O(N); SC: O(1)
+# end
