1cb1079e14c2a397f07c5643fe5e19859e05be8c
Animesh Okhade <96253880+animeshokhade@users.noreply.github.com>
2022-05-27 18:20:43 +0530

    Add files via upload

---
+++ geeks4geeks/#52.py
@@ -0,0 +1,9 @@
+# question --> https://practice.geeksforgeeks.org/problems/mean0021/1/?page=2&difficulty[]=-2&status[]=unsolved&sortBy=submissions#
+
+class Solution:
+    def mean(self, N, A):
+        # code here
+        return sum(A) // N
+
+    # TC: O(N); SC: O(1)
+# end
