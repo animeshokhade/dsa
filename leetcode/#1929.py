27bf5de60f12b4dd79f2b1065739473eb1e79d52
Animesh Okhade <96253880+animeshokhade@users.noreply.github.com>
2022-05-30 21:22:10 +0530

    Add files via upload

---
+++ leetcode/#1929.py
@@ -0,0 +1,7 @@
+# question --> https://leetcode.com/problems/concatenation-of-array/
+
+class Solution:
+    def getConcatenation(self, nums: List[int]) -> List[int]:
+        return nums * 2
+
+# TC: O(N); SC: O(1)
