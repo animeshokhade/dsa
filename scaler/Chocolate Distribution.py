cd73ad9a87c7ab88d0ff1f0847a1692aaee9b2fc
Animesh Okhade <96253880+animeshokhade@users.noreply.github.com>
2022-05-30 18:35:24 +0530

    Add files via upload

---
+++ scaler/Chocolate Distribution.py
@@ -0,0 +1,18 @@
+class Solution:
+    # @param A : list of integers
+    # @param B : integer
+    # @return an integer
+    def solve(self, A, B):
+        if not A or B == 0: return 0
+        A.sort()
+        minDiff = float('inf')
+        window = len(A) - B + 1
+        for diff in range(window):
+            minDiff = min(minDiff, A[diff + B - 1] - A[diff])
+        return minDiff
+
+
+'''
+TC: O(N + logN) => O(N)
+SC: O(1)
+'''
