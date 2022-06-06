c3a5777e060d55c6e5a77bd84d60e31d405b5a40
Animesh Okhade <animeshokhade2020@gmail.com>
2022-06-06 09:43:15 +0530

    Add files via upload

---
+++ scaler/Single Element in a Sorted Array.py
@@ -0,0 +1,33 @@
+class Solution:
+    # @param A : list of integers
+    # @return an integer
+    def solve(self, A):
+        n = len(A)
+        low = 0
+        high = n - 1
+
+        # boundary conditions
+        if high == 0:
+            return A[0]
+
+        # the two conditions below are necessary because, the
+        # the algorithm based on pattern of indices will only
+        # work if the element occuring only once is in the middle.
+        if A[0] != A[1]:
+            return A[0]
+        if A[-1] != A[-2]:
+            return A[-1]
+
+            # binary search
+        while low <= high:
+            mid = (low + high) // 2
+            if A[mid] != A[mid + 1] and A[mid] != A[mid - 1]:
+                return A[mid]
+            # conditions in which we must discard the right searchspace
+            elif (mid & 1 == 0 and (A[mid] == A[mid - 1])) or (mid & 1 == 1 and (A[mid] == A[mid + 1])):
+                high = mid - 1
+            # discard the left searchspace
+            else:
+                low = mid + 1
+
+        # TC: O(logN); SC: O(1)
