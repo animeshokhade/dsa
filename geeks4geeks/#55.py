d76001d4f7c826a8b4ea1f2dad6afb0d8de56ed8
Animesh Okhade <animeshokhade2020@gmail.com>
2022-06-07 17:26:16 +0530

    Add files via upload

---
+++ geeks4geeks/#55.py
@@ -0,0 +1,8 @@
+# question --> https://practice.geeksforgeeks.org/problems/the-dice-problem2316/1/?page=1&difficulty[]=-2&status[]=unsolved&sortBy=submissions#
+
+class Solution:
+    def oppositeFaceOfDice(self, N):
+        # code here
+        return 7 - N
+
+    # TC: O(1); SC: O(1)
