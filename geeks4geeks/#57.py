933c5b51045c5c2194ba23aeeb819d27cd9eff5f
Animesh Okhade <animeshokhade2020@gmail.com>
2022-06-07 17:49:21 +0530

    Add files via upload

---
+++ geeks4geeks/#57.py
@@ -0,0 +1,17 @@
+class Solution:
+    def binarysearch(self, arr, n, k):
+        # code here
+        low = 0
+        high = n - 1
+        while low <= high:
+            mid = (low + high) // 2
+            if arr[mid] == k:
+                return mid
+            elif arr[mid] < k:
+                low = mid + 1
+            else:
+                high = mid - 1
+
+        return -1
+
+    # TC: O(logN); SC: O(1)
