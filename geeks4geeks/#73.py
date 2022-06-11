97086bf1034d3247561eb009915aadb9c6f10cd3
Animesh Okhade <animeshokhade2020@gmail.com>
2022-06-11 17:45:24 +0530

    Add files via upload

---
+++ geeks4geeks/#73.py
@@ -0,0 +1,8 @@
+# question --> https://practice.geeksforgeeks.org/problems/lower-case-to-upper-case3410/1/?page=1&difficulty[]=-2&status[]=unsolved&sortBy=submissions#
+
+class Solution:
+    def to_upper(self, str):
+        # code here
+        return str.upper()
+
+    # TC: O(|s|); SC: O(1)
