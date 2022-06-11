309225b60e4a57c49286138a7320649c33538ac2
Animesh Okhade <animeshokhade2020@gmail.com>
2022-06-11 16:48:18 +0530

    Add files via upload

---
+++ geeks4geeks/#70.py
@@ -0,0 +1,8 @@
+# question --> https://practice.geeksforgeeks.org/problems/simple-interest3457/1/?page=3&difficulty[]=-2&status[]=unsolved&sortBy=submissions#
+
+class Solution:
+    def simpleInterest(self, A, B, C):
+        # code here
+        return (A * B * C) / 100
+
+    # TC: O(1); SC: O(1)
