21647fb8874dc104ec9c55cad7a6a74192272bf3
Animesh Okhade <animeshokhade2020@gmail.com>
2022-06-13 18:47:15 +0530

    Add files via upload

---
+++ geeks4geeks/#80.py
@@ -0,0 +1,8 @@
+# question --> https://practice.geeksforgeeks.org/problems/greatest-of-three-numbers2520/1/?page=1&difficulty[]=-2&status[]=unsolved&sortBy=submissions#
+
+class Solution:
+    def greatestOfThree(self, A, B, C):
+        # code here
+        return max(A, B, C)
+
+    # TC: O(1); SC: O(1)
