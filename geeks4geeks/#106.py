7d127e771c37107b7218ddc1eee38b7bff49f79c
Animesh Okhade <animeshokhade2020@gmail.com>
2022-06-14 14:09:29 +0530

    Add files via upload

---
+++ geeks4geeks/#106.py
@@ -0,0 +1,8 @@
+# question --> https://practice.geeksforgeeks.org/problems/cube-root-of-a-number0915/1/?page=2&difficulty[]=-2&status[]=unsolved&sortBy=submissions
+
+class Solution:
+    def cubeRoot(self, N):
+        # code here
+        return int(pow(N, 1 / 3))
+
+    # TC: O(cube root of N); SC: O(1)
