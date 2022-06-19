99708d9aa193ae663701e4df531c0e49ec733d91
Animesh Okhade <animeshokhade2020@gmail.com>
2022-06-19 08:16:26 +0530

    Add files via upload

---
+++ hackerrank/introduction_to_sets.py
@@ -0,0 +1,14 @@
+# question --> https://www.hackerrank.com/challenges/py-introduction-to-sets/problem?isFullScreen=false
+
+def average(array):
+    # your code goes here
+    avg = sum(set(arr)) / len(set(arr))
+    return avg
+
+
+if __name__ == '__main__':
+    n = int(input())
+    arr = list(map(int, input().split()))
+    result = average(arr)
+    print(result)
+    
