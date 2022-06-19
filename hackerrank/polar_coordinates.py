cc476415b7d8ad7920136db920f3e0eaf606d955
Animesh Okhade <animeshokhade2020@gmail.com>
2022-06-19 12:01:45 +0530

    Add files via upload

---
+++ hackerrank/polar_coordinates.py
@@ -0,0 +1,9 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+
+from cmath import phase
+
+cnum = complex(input())
+
+print(abs(complex(cnum.real, cnum.imag)))
+print(phase(complex(cnum.real, cnum.imag)))
+
