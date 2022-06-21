1e6a576a1d57651716f76fcb3f66a0b7332e6e63
Animesh Okhade <animeshokhade2020@gmail.com>
2022-06-21 11:50:57 +0530

    Add files via upload

---
+++ geeks4geeks/#144.py
@@ -0,0 +1,7 @@
+# question --> https://practice.geeksforgeeks.org/problems/implement-strstr/1/?page=2&difficulty[]=-1&difficulty[]=0&status[]=unsolved&sortBy=submissions
+
+def strstr(s, x):
+    # code here
+    return s.find(x)
+
+    # TC: O(|S| * |X|): SC: O(1)
