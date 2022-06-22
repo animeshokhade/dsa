653ff79c8cc2cf66646ea4f1c6f530699f5cba25
Animesh Okhade <animeshokhade2020@gmail.com>
2022-06-22 17:37:51 +0530

    Add files via upload

---
+++ geeks4geeks/#164.py
@@ -0,0 +1,7 @@
+# question --> https://practice.geeksforgeeks.org/problems/sum-of-array-elements2502/1/?page=4&difficulty[]=-1&difficulty[]=0&status[]=unsolved&sortBy=submissions
+
+def sumElement(arr,n):
+    #code here
+    return sum(arr)
+
+    # TC: O(N); SC: O(1)
