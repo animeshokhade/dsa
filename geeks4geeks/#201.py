f176ea7004a39c8f8cfd19080758f5fdd40c705c
Animesh Okhade <animeshokhade2020@gmail.com>
2022-06-23 20:02:50 +0530

    Add files via upload

---
+++ geeks4geeks/#201.py
@@ -0,0 +1,9 @@
+# question --> https://practice.geeksforgeeks.org/problems/sort-the-array0055/1/?page=2&difficulty[]=-1&status[]=unsolved&sortBy=submissions#
+
+class Solution:
+    def sortArr(self, arr, n): 
+        #code here
+        arr.sort()
+        return arr
+        
+        # TC: O(NlogN); SC: O(1)
