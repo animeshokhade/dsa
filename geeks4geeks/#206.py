9614ebb230d9d7a9b2659ea26a1ae309ee6d1028
Animesh Okhade <animeshokhade2020@gmail.com>
2022-06-23 20:48:24 +0530

    Add files via upload

---
+++ geeks4geeks/#206.py
@@ -0,0 +1,7 @@
+# question --> https://practice.geeksforgeeks.org/problems/euler-totient-sum-and-divisors5501/1/?page=26&difficulty[]=-1&status[]=unsolved&sortBy=submissions#
+
+class Solution:
+    def phiSum (self, N):
+        # code here
+        return N
+        # TC: O(1); SC: O(1)
