651ed764da0d0029950c6dc9e982d0c949463f56
Animesh Okhade <animeshokhade2020@gmail.com>
2022-06-25 18:04:13 +0530

    Add files via upload

--- leetcode/#5.py
+++ leetcode/#5.py
@@ -34,4 +34,80 @@ class Solution:
         
         # TC: O(N^2); SC: O(N)
         
-# end 
+# cleaner BF
+
+class Solution:
+    def isPal(self, s: str, n: int, l: int, r: int) -> Tuple [int, int]:
+        while l >= 0 and r < n: 
+            if s[l] != s[r]:
+                break
+            l -= 1
+            r += 1
+            
+        return l + 1, r - 1
+    
+    def longestPalindrome(self, s: str) -> str:
+        if not s: 
+            return ' '
+        
+        n = len(s)
+        
+        l, r = 0, 0
+        pal = s[0]
+        
+        for i in range(n):
+            l1, r1 = self.isPal(s, n, i, i)
+            if r1 - l1 > r - l: 
+                pal = s[l1:r1 + 1]
+                l, r = l1, r1
+            
+            if i < n - 1 and s[i] == s[i + 1]:
+                l1, r1 = self.isPal(s, n, i, i + 1)
+                if r1 - l1 > r - l: 
+                    pal = s[l1:r1 + 1]
+                    l, r = l1, r1
+                    
+        return pal 
+    
+        # TC: O(|S|^2); SC: O(|S|)
+                
+        
+# optimisation
+
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        t = '#'.join('@' + s + '$')
+        n = len(t)
+        
+        pal_len = [0] * n
+        centre = 0
+        
+        for i in range(1, n - 1):
+            right_boundary = centre + pal_len[centre]
+            d = i - centre
+            mirror_index = centre - d
+            
+            pal_len[i] = right_boundary > i and \
+                min(pal_len[mirror_index], right_boundary - i)
+            
+            # if pal_len[i] is updated with right_boundary - i
+            # then there is a possibility of having a larger
+            # palindrome size as the right boundary of the 
+            # palidrome centred at centre overlaps with the 
+            # right boundary of the sub-palindrome. 
+            
+            # expanding palindrome centred at i
+            while t[i + pal_len[i] + 1] == t[i - pal_len[i] - 1]:
+                pal_len[i] += 1
+                
+            # if palindrome centred at i expands past 
+            # right_boundary, we update centre as the LPS
+            # is now centred at i
+            if pal_len[i] + i > right_boundary:
+                centre = i 
+                
+        # LPS Centre and max_pal
+        max_pal, lps_cen = max((pal, cen) \
+                              for cen, pal in enumerate(pal_len))
+        
+        return s[(lps_cen - max_pal) // 2:(lps_cen + max_pal) // 2]
