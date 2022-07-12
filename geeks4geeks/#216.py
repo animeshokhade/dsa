23858dd9f1a4d48c715c52cb71d9169b98ec612b
Animesh Okhade <animeshokhade2020@gmail.com>
2022-07-12 21:01:06 +0530

    Add files via upload

---
+++ geeks4geeks/#216.py
@@ -0,0 +1,8 @@
+# question --> https://practice.geeksforgeeks.org/problems/fighting-the-darkness3949/1/?page=1&difficulty[]=-1&status[]=unsolved&sortBy=submissions#
+
+class Solution:
+    def maxDays(self, arr, n):
+        # code here
+        return max(arr)
+
+        # TC: O(N); SC: O(1)
