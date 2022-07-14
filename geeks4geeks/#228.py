b4a050463fcc1de5c18cddd3219d28e4bede00ef
Animesh Okhade <animeshokhade2020@gmail.com>
2022-07-14 09:15:41 +0530

    Add files via upload

---
+++ geeks4geeks/#228.py
@@ -0,0 +1,16 @@
+# question --> https://practice.geeksforgeeks.org/problems/immediate-smaller-element1142/1/?page=2&difficulty[]=-1&status[]=unsolved&sortBy=submissions#
+
+class Solution:
+
+    def immediateSmaller(self, arr, n):
+        # code here
+        for i in range(n - 1):
+            if arr[i] > arr[i + 1]:
+                arr[i] = arr[i + 1]
+            else:
+                arr[i] = -1
+
+    arr[-1] = -1
+    return arr
+
+    # TC: O(N); SC: O(1)
