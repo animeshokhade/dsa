6696e3fa59fba0630d994d085508d8c42db1424d
Animesh Okhade <animeshokhade2020@gmail.com>
2022-07-15 22:12:57 +0530

    Add files via upload

---
+++ geeks4geeks/#240.py
@@ -0,0 +1,11 @@
+# question --> https://practice.geeksforgeeks.org/problems/remainder-with-7/1/?page=1&difficulty[]=-1&status[]=unsolved&category[]=Strings&sortBy=submissions#
+
+class Solution:
+    # your task is to complete this function
+    # Function should return the required answer
+    # You are not allowed to convert string to integer
+    def remainderWith7(self, str):
+        # Code here
+        return int(str) % 7
+
+        # TC: O(1); SC: O(1) 
