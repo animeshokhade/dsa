1c0fc082503a22e6c9fa529ef439f6e4697baeb4
Animesh Okhade <animeshokhade2020@gmail.com>
2022-07-15 14:13:14 +0530

    Add files via upload

--- scaler/Evaluate Expression.py
+++ scaler/Evaluate Expression.py
@@ -33,3 +33,37 @@ class Solution:
 
     # TC: O(N); SC: O(Operands)
     # N -> Array length
+
+# different style of writing
+
+class Solution:
+    # @param A : list of strings
+    # @return an integer
+    def evaluate(self, num1, num2, opr):
+        if opr == '+':
+            return num2 + num1
+        elif opr == '-':
+            return num2 - num1
+        elif opr == '*':
+            return num2 * num1
+        else:
+            return num2 // num1
+
+
+    def evalRPN(self, A):
+        stack = []
+        ret = 0
+        opr = {'+', '-', '*', '/'}
+
+        for ind, ele in enumerate(A):
+            if ele in opr and stack:
+                num1 = int(stack.pop())
+                num2 = int(stack.pop())
+                ret = self.evaluate(num1, num2, ele)
+                stack.append(ret)
+            else:
+                stack.append(ele)
+
+        return stack[-1]
+
+# TC: O(N); SC: O(N)
