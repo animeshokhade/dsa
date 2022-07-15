59459233f628138cf6092367691876ef90d3c9b5
Animesh Okhade <animeshokhade2020@gmail.com>
2022-07-15 22:49:13 +0530

    Add files via upload

---
+++ geeks4geeks/#243.py
@@ -0,0 +1,12 @@
+# question --> https://practice.geeksforgeeks.org/problems/change-the-string3541/1/?page=1&difficulty[]=-1&status[]=unsolved&category[]=Strings&sortBy=submissions#
+
+class Solution:
+    def modify(self, s):
+        # code here
+        if s[0].islower():
+            return s.lower()
+
+        else:
+            return s.upper()
+
+    # TC: O(|s|); SC: O(|s|) 
