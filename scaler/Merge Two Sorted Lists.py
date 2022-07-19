c7735c2b73fd45430b697b9e52daeb531bd373b2
Animesh Okhade <animeshokhade2020@gmail.com>
2022-07-19 23:38:43 +0530

    Update Merge Two Sorted Lists.py

--- scaler/Merge Two Sorted Lists.py
+++ scaler/Merge Two Sorted Lists.py
@@ -9,29 +9,29 @@ class Solution:
 	# @param B : head node of linked list
 	# @return the head node in the linked list
 	def mergeTwoLists(self, A, B):
-        dummy = ListNode(0)
-        temp = dummy
+        	dummy = ListNode(0)
+        	temp = dummy
 
-        while A and B:
-            if A.val < B.val:
-                temp.next = A
-                temp = temp.next
-                A = A.next
-            else:
-                temp.next = B
-                temp = temp.next
-                B = B.next
+        	while A and B:
+	            if A.val < B.val:
+        	        temp.next = A
+                	temp = temp.next
+	                A = A.next
+        	    else:
+                	temp.next = B
+	                temp = temp.next
+        	        B = B.next
 
-        while A:
-            temp.next = A
-            temp = temp.next
-            A = A.next
+	        while A:
+        	    temp.next = A
+	            temp = temp.next
+        	    A = A.next
 
-        while B:
-            temp.next = B
-            temp = temp.next
-            B = B.next
+	        while B:
+        	    temp.next = B
+	            temp = temp.next
+        	    B = B.next
 
-        return dummy.next
+	        return dummy.next
 
         # TC: O(A + B); SC: O(1) 
